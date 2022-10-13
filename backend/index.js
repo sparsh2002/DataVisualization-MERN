b466b3f7da96492bb52dfc537b567b8248b1fdc6
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2022-10-13 22:05:18 +0530

    provide data added to the database

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,18 @@ const cors = require('cors')
 const path = require('path')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser');
+const Reports = require('./models/ReportSchema')
+const fs = require('fs')
+const mongoose = require('mongoose')
 require("dotenv").config();
 const app = express()
+app.use(express.json())
 const PORT = process.env.PORT
 
 const db = require('./db.js')
 
 db.connect()
-// const router = require('./routes/index');
+const router = require('./routes/index');
 
 // middle ware
 app.use(bodyParser.json({limit:"50mb"}))
@@ -38,14 +42,27 @@ app.use(cookieParser());
 //     next()
 // })
 
-// app.use("/api/v1" , router)
-
-// // firebase
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://fir-project-1-58a04.firebaseio.com"
-// });
+app.use("/api/v1" , router)
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
 })
+
+
+
+// const data = JSON.parse(fs.readFileSync('./data.json', 'utf-8'))
+
+// const importData = async (data) => {
+//     try {
+//     // console.log(Reports)
+//     // console.log(data)
+//       await Reports.create(data)
+//       console.log('data successfully imported')
+//       // to exit the process
+//       process.exit()
+//     } catch (error) {
+//       console.log('error ->>> ', error)
+//     }
+// }
+
+// importData(data)
---
+++ backend/models/ReportSchema.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ReportSchema = new Schema({
+    end_year: {
+        type:String,
+    },
+
+    intensity:{
+        type : Number,
+    },
+    sector: {
+        type : String,
+    },
+    topic: {
+        type : String,
+    },
+    insight: {
+        type: String,
+    },
+    url: {
+        type : String,
+    },
+    region: {
+        type : String,
+    },
+    start_year : {
+        type : String
+    },
+    impact: {
+        type : String,
+    },
+    added: {
+        type : String,
+    },
+    published: {
+        type : String,
+    },
+    country : {
+        type : String,
+    },
+    relevance: {
+        type : Number,
+    },
+    pestle: {
+        type : String,
+    },
+    source: {
+        type : String,
+    },
+    title : {
+        type : String,
+    },
+    likelihood : {
+        type : Number
+    },
+})
+
+
+const Reports = mongoose.model('Report' , ReportSchema)
+
+module.exports = Reports
