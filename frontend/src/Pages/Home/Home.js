16d6f98fcc0b94c021816cb4ab001693caa9d984
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2022-10-13 12:57:41 +0530

    google charts connected

---
+++ frontend/src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import BarChart from '../../components/BarChart'
+
+
+function Home() {
+  return (
+    <div>
+        <BarChart />
+    </div>
+  )
+}
+
+export default Home
